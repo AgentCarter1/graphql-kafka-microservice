6a7533cfd242f2cc75ee519acdc6112c6b950e24
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2024-08-08 11:14:54 +0000

    kafka

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -3,12 +3,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
-
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/users.schema.gql'),
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     UsersModule,
   ],
