b0b6986746734125ed2443869e6dacd37d222bee
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2024-08-08 08:41:49 +0000

    changed name

--- users-organizations/src/app.controller.spec.ts
+++ users-organizations/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './users-organizations.controller';
+import { AppService } from './users-organizations.service';
 
 describe('AppController', () => {
   let appController: AppController;
--- users-organizations/src/users-organizations.module.ts
+++ users-organizations/src/users-organizations.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './users-organizations.controller';
+import { AppService } from './users-organizations.service';
 
 @Module({
   imports: [],
