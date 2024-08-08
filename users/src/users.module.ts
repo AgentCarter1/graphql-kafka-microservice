5ab8245a0db161fc56f23ab5ff80a655d58fbd73
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2024-08-08 08:52:42 +0000

    fix

--- users/src/users.module.ts
+++ users/src/users.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './users.controller';
 import { AppService } from './users.service';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
